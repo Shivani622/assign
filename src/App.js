1ba24065cf40e866ea325d2f49b880d9c7d97571
Shivani <shivani080202@gmail.com>
2024-10-02 17:33:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import KanbanBoard from './KanbanBoard';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h3></h3>
+      <KanbanBoard />
+    </div>
+  );
+};
+
+export default App;
